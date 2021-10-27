c0023488c6e928fed983c53696f91557b9097154
fenrir4ik <emil.nadzhiev@gmail.com>
2021-10-27 03:08:18 +0300

    Auth settings update
    rest_framework.authtoken deleted

--- computershop/settings.py
+++ computershop/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'apps.api_user',
     'apps.api_product',
     'rest_framework',
-    'rest_framework.authtoken',
     'knox',
 ]
 
@@ -147,8 +146,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
         'knox.auth.TokenAuthentication',
     ]
 }
