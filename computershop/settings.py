5f9510ee367144597478d118b236aa426db1978d
fenrir4ik <emil.nadzhiev@gmail.com>
2021-11-02 20:47:03 +0200

    Log in default DRF fix
    REST_FRAMEWORK = {
        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
        'PAGE_SIZE': 10,
        'DEFAULT_AUTHENTICATION_CLASSES': (
            'knox.auth.TokenAuthentication',
            'rest_framework.authentication.TokenAuthentication',   #added
            'rest_framework.authentication.SessionAuthentication', #added
        )
    }

--- computershop/settings.py
+++ computershop/settings.py
@@ -147,9 +147,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'knox.auth.TokenAuthentication',
-    ]
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
 
 REST_KNOX = {'TOKEN_TTL': timedelta(hours=24)}
