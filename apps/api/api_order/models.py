1bb35d564692d5e9abd8e96ec8141f71136499dc
fenrir4ik <emil.nadzhiev@gmail.com>
2021-12-01 14:50:56 +0200

    Payments added

--- apps/api/api_order/models.py
+++ apps/api/api_order/models.py
@@ -11,12 +11,23 @@ class OrderStatus(models.Model):
         return self.status_name
 
 
+class PaymentType(models.Model):
+    class Meta:
+        db_table = 'payment_type'
+
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.type
+
+
 class Order(models.Model):
     class Meta:
         db_table = 'order'
 
     user = models.ForeignKey(User, models.RESTRICT)
     order_status = models.ForeignKey(OrderStatus, models.RESTRICT, null=True)
+    payment_type = models.ForeignKey(PaymentType, models.RESTRICT, null=False)
     order_date = models.DateField(blank=True, null=True, db_index=True)
     to_name = models.CharField(max_length=30, db_index=True)
     to_telno = models.CharField(max_length=30, db_index=True)
