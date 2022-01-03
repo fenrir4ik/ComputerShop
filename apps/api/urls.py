7ca405da021b77c1cec3593f9515546b2520ffa6
fenrir4ik <emil.nadzhiev@gmail.com>
2022-01-03 17:37:34 +0200

    api_shopping_cart url changes

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('api_user/', include(api_user_urlpatterns)),
     path('api_product/', include(api_product_urlpatterns)),
     path('api_order/', include(api_order_urlpatterns)),
-    path('shopping_cart/', include(api_shopping_cart_urlpatterns)),
+    path('api_shopping_cart/', include(api_shopping_cart_urlpatterns)),
     path('', ApiRepository.as_view(), name='API Repository'),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
