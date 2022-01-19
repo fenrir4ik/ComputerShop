0ef8d0047fe0c90143b4e89f4f48a326b020c389
fenrir4ik <emil.nadzhiev@gmail.com>
2022-01-20 00:30:09 +0200

    User model added to admin

---
+++ apps/user/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from apps.user.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'email', 'phone_number', 'date_joined')
+    search_fields = ('email', 'phone_number')
+    readonly_fields = ('date_joined', 'last_login')
+    filter_horizontal = ()
+    list_filter = ('date_joined',)
+    fieldsets = ()
+    ordering = ()
--- computershop/urls.py
+++ computershop/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('apps.store.urls')),
     path('user/', include('apps.user.urls')),
 ]
