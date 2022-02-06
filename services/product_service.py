e1bfc23fa3d9aee8ea01f03354f9ac3d7a35f501
fenrir4ik <emil.nadzhiev@gmail.com>
2022-02-06 20:42:51 +0200

    Update product_service.py

--- services/product_service.py
+++ services/product_service.py
@@ -5,12 +5,6 @@ from apps.store.models import Product, ProductPrice, ProductImage
 
 class ProductRetrieveService():
     def get_products_list(self, include_price=True, include_image=True):
-        # actual_price = ProductPrice.objects.filter(product=OuterRef('pk')).order_by('-date_actual')
-        # product_image = ProductImage.objects.filter(Q(product=OuterRef('pk')) & Q(is_main=True))
-        # products = Product.objects.select_related('vendor', 'category') \
-        #     .annotate(price=Subquery(actual_price.values('price')[:1])) \
-        #     .annotate(image=Subquery(product_image.values('image')[:1]))
-        # return products
         products = Product.objects.select_related('vendor', 'category')
         if include_price:
             actual_price = ProductPrice.objects.filter(product=OuterRef('pk')).order_by('-date_actual')
@@ -28,15 +22,16 @@ class ProductCreateService:
 
 class ProductUpdateService:
     def update_product_images(self, product, image_list):
-        # ProductImage.objects.filter(product=product).delete()
         is_main_image = True
         for image_form in image_list:
+
+            # check if image field was submitted (required=False make it false)
             if image_form.cleaned_data:
-                delete_image = image_form.cleaned_data.get('DELETE')
-                if delete_image:
-                    id_instance = image_form.cleaned_data.get('id')
-                    if id_instance:
-                        ProductImage.objects.get(pk=id_instance.id).delete()
+                old_image_instance = image_form.cleaned_data.get('id')
+
+                # check if this image should be deleted
+                if image_form.cleaned_data.get('DELETE') and old_image_instance:
+                    old_image_instance.delete()
                 else:
                     product_image = image_form.save(commit=False)
                     if product_image.image:
@@ -44,6 +39,7 @@ class ProductUpdateService:
                         product_image.is_main = is_main_image
                         product_image.save()
                         is_main_image = False
+        # if during update process image wasn't set create default image instance
         if is_main_image:
             ProductImage(product=product, is_main=True).save()
 
