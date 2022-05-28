324f4d2ba6e1857a9d836e2299c3e0a2bf1d1922
SonyaEr <emil.nadzhiev@gmail.com>
2022-05-28 20:39:59 +0300

    Silk settings

--- computershop/urls.py
+++ computershop/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('admin-panel/', include('apps.admin_panel.urls')),
 ]
 
-urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
+# urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
